9aa3c10f2a2e94bdf9e5e4cda3d1299b085142dd
Dagmara Daszkiewicz <dagmara.daszkiewicz@gmail.com>
2018-02-13 00:02:14 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+
+gulp.task('sass',function(){
+    return gulp.src('./scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(autoprefixer())
+        .pipe(sass({
+                errLogToConsole: true,
+                outputStyle: 'expanded'
+            }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./css'))
+});
+
+gulp.task('default',function(){
+    gulp.watch('./scss/**/*.scss',['sass']);
+});
