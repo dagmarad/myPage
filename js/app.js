9398faa2f36c093fb907f924487e417a12d428a9
Dagmara Daszkiewicz <dagmara.daszkiewicz@gmail.com>
2018-02-15 16:12:36 +0100

    add paralax effect for one div:

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 $(function(){
 
+window.addEventListener("scroll", (function(event){
+parallaxScroll();
 
 
+}))
+let parralax1 = document.querySelector('.parallax-1');
+function parallaxScroll(){
+  var scrolled = document.documentElement.scrollTop;
+  let cos =parralax1.style.bottom=(0-(scrolled*.2)+'px');
+  }
 
 });
