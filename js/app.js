d6887ad0695e2c89072abe5afa5ac5359ceda52b
Dagmara Daszkiewicz <dagmara.daszkiewicz@gmail.com>
2018-02-15 17:37:26 +0100

    add third parralax

--- js/app.js
+++ js/app.js
@@ -6,9 +6,16 @@ parallaxScroll();
 
 }))
 let parralax1 = document.querySelector('.parallax-1');
+let parralax2 = document.querySelector('.parallax-2');
+let parralax3 = document.querySelector('.parallax-3');
 function parallaxScroll(){
   var scrolled = document.documentElement.scrollTop;
-  let cos =parralax1.style.bottom=(0-(scrolled*.2)+'px');
+  parralax1.style.bottom=(0-(scrolled*1.2)+'px');
+  parralax2.style.bottom=(0-(scrolled*1.4)+'px');
+  parralax2.style.left=(0-(scrolled*.4)+'px');
+  parralax3.style.transition=(-(scrolled+3-scrolled)+'s');
   }
+
+
 
 });
